b54355e49371f7664b395ac2681b561a1b2ec45a
Pooja Yadav <poojayadav190618@gmail.com>
2026-04-14 23:02:57 +0530

    react day 2

--- App.js
+++ App.js
@@ -1,4 +1,38 @@
-const heading = React.createElement("h1",{id:"heading"},"Hello world from React!");
+/**
+ * 
+ * <div id ="parent">
+ *              <Div id="child">
+ *              <h1> </h1>
+ *              </div>
+ * 
+ * </Div>
+ * 
+ * 
+ */
+
+
+const parent = React.createElement(
+        "div",
+        {id: "parent"},
+        React.createElement(
+                "div",
+                {id:  "child"},
+                React.createElement("h1", {}, "Hello, i am h1")
+        )
+);
+
+
+
+
+
+
+// const heading = React.createElement(
+//         "h1",
+//         {id:"heading"},"Hello world from React!"
+// );
+// console.log(heading);
+
         const root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(heading);
+
+        root.render(parent);
 
